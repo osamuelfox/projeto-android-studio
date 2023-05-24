6e240d04e252f6acf099e55eec4e8cecfc2c6e09
osamuelfox <samuelsantosdesouza92@gmail.com>
2023-05-23 21:46:01 -0300

    Atualizando IMC

---
+++ Anotacao/app/src/main/java/com/example/anotacao/model/Pessoa.java
@@ -0,0 +1,8 @@
+package com.example.anotacao.model;
+
+public class Pessoa {
+    
+
+
+
+}
---
+++ IMC/app/src/main/java/com/example/imc/view/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.imc.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import com.example.imc.R;
+import com.example.imc.model.Pessoa;
+
+public class MainActivity extends AppCompatActivity {
+//
+//    Pessoa pessoa;
+//
+//    EditText editPeso;
+//    EditText editAltura;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//
+//        pessoa = new Pessoa();
+//        pessoa.setPeso("65");
+//        pessoa.setAltura("170");
+//
+//        editPeso = findViewById(R.id.text_peso);
+//        editAltura = findViewById(R.id.text_altura);
+//
+//        editPeso.setText(pessoa.getPeso());
+//        editAltura.setText(pessoa.getAltura());
+//
+//        Log.i("ProgramacaoPOO", pessoa.toString());
+//        Log.i("ProgramacaoPOO", pessoa.toString());
+    }
+}
